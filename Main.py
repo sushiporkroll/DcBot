889c8b70d80f892bf3e9af2fb0953f4ee5398a5c
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-09 17:53:01 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,23 +6,22 @@ import inspect
 import time
 import os
 
-client = discord.Client()
-client = commands.Bot(command_prefix = "/")
+bot = commands.Bot(command_prefix = "/")
 
-@client.event
+@bot.event
 async def on_ready():
     print("Bot is in my penis!")
 
 def user_is_me(ctx):
     return ctx.message.author.id == "303657894161809412"
 
-@client.command(name='eval', pass_context=True)
+@bot.command(name='eval', pass_context=True)
 @commands.check(user_is_me)
 async def _eval(ctx, *, command):
     res = eval(command)
     if inspect.isawaitable(res):
-        await client.say(await res)
+        await bot.say(await res)
     else:
-        await client.say(res)
+        await bot.say(res)
 
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
