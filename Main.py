eb5c8a675df647907300ffc5a0b9cf0a25a03763
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-01-09 18:03:38 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,16 +14,15 @@ async def on_ready():
     print(bot.user.id)
 
 def user_is_me(ctx):
-    return ctx.message.author.id == "277983178914922497"
-	
+	return ctx.message.author.id == "277983178914922497"
+
 @bot.command(name='eval', pass_context=True)
 @commands.check(user_is_me)
 async def _eval(ctx, *, command):
-    res = eval(command)
-    if inspect.isawaitable(res):
-        await bot.say(await res)
-    else:
-        await bot.delete_message(ctx.message)
-        await bot.say(res)
+	res = eval(command)
+	if inspect.isawaitable(res):
+		await bot.say(await res)
+	else:
+		await bot.say(res)
 
 bot.run(os.environ['BOT_TOKEN'])
